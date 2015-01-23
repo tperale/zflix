ccdd7028cf33a40963f60d0b22bf6184070a4c15
thomacer <tperale@ulb.ac.be>
2015-01-23 22:17:38 +0100

    Removed the deprecated version of BeautifulSoup module and use bs4 instead. Removed the 'feedparser' module to have less dependencies.

--- zflix.py
+++ zflix.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import urllib
-import feedparser
-import BeautifulSoup
+import bs4
 import sys
 import os
 import argparse
@@ -12,34 +11,30 @@ import subprocess
 
 # Constant
 
-
 class AppURLopener(urllib.FancyURLopener):
     version = "Mozilla/5.0 (X11; U; Linux x86_64; en-US)" + \
         "AppleWebKit/532.0 (KHTML, like Gecko) Chrome/4.0.202.0 Safari/532.0"
 
 urllib._urlopener = AppURLopener()
 
-DEBUG = 0
-string_max = 50
-# destdir = '~/Downloads'
 destdir = os.path.expanduser('~/Downloads')
 
 locations = {"h33t": {"url": "http://www.h33t.to",
                       "dl": "h33t.to/get/"},
              "tpb": {"url": "http://thepiratebay.org",
                      "dl": "http://torrents.thepiratebay.org"},
-             "bitsnoop": {"url": "http://bitsnoop.com",
-                          "dl": "http://torrage.com/torrent/.*?\.torrent"},
+             "isohunt": {"url": "http://isohunt.to",
+                         "dl": "torrent.isohunt.to/download.php?id="},
              "torrentfunk.com": {"url": "http://www.torrentfunk.com",
-                                 "dl": "www.torrentfunk.com/tor/*.torrent"},
+                                 "dl": ".torrent"},
              "limetorrents.cc": {"url": "http://www.limetorrents.cc",
                                  "dl": "itorrents.org/torrent/"},
-             "torrents.net": {"url": "http://www.torrents.net",
-                              "dl": "www.torrents.net/down/*.torrent"},
+#             "torrents.net": {"url": "http://www.torrents.net",
+#                              "dl": ".torrent"},
              "vertor": {"url": "http://www.vertor.com",
-                        "dl": "http://www.vertor.com/.*?mod=download.*?id="},
+                        "dl": "?mod=download."},
              "torlock": {"url": "www.torlock.com",
-                         "dl": "www.torlock.com/tor/*.torrent"}
+                         "dl": ".torrent"}
 
              }
 
@@ -72,6 +67,7 @@ def save_file(toSave, outputPath):
     """
     Save a webpage in a specified file
     """
+
     try:
         print('Writting')
         with open(outputPath, "w") as openedFile:
@@ -97,26 +93,31 @@ def gethref(page, toFind):
     page: Source code of the page where you want to find 'toFind'
     """
 
-    if DEBUG:
-        print("gethref(%s, %s)" % (toFind, page))
+    soup = bs4.BeautifulSoup(page)
+    urls = soup.find_all('a')  # 'urls' contain a list of <a href=''>...</a>
 
-    soup = BeautifulSoup.BeautifulStoneSoup(page)
-    urls = soup('a')  # 'urls' contain a list of <a href=''>...</a>
-
-    match = (toFind in urls[0]['href'])
-    i = 1
-    while not(match) and i < len(urls):
-        match = (toFind in urls[i]['href'])
-
-        if DEBUG:
-            print("match: %s pour link: %s" % (match, urls[i]['href']))
-
-        i += 1
+    href = urls[0].get('href')
+    if href is not None:
+        match = (toFind in urls[0].get('href'))
     else:
         match = False
+    i = 0
+
+    while not(match) and i < (len(urls) - 1):
+        i += 1
+
+        if DEBUG:
+            print(urls[i])
+
+        href = urls[i].get('href')
+        if href is not None:
+            match = (toFind in href)
+
+        if DEBUG:
+            print("match: %s pour link: %s" % (toFind, urls[i].get('href')))
 
     # link['href'] is what's it is after href='...'
-    return urls[i]['href'] if match else False
+    return urls[i].get('href') if match else False
 
 
 def get_page(basePage, researchedUrl):
@@ -164,31 +165,39 @@ def locationTesting(trackersPage, outputPath, title):
 
 def main(option, domain):
     search = option.search.replace(' ', '+')
-#    torrentzPage = urllib.urlopen(domain + search).read()
     torrentzPage = urllib.urlopen(domain + '?q=' + search).read()
-    feed = feedparser.parse(torrentzPage)
-    item_num = feed["items"].__len__()
+    feed = bs4.BeautifulSoup(torrentzPage)
+    feedTitle = feed.find_all('title')
+    feedDescription = feed.find_all('description')
+
+    print(bcolors.HEADER + feedTitle[0].get_text() + bcolors.ENDC)
+    feedTitle.pop(0)
+    print(bcolors.HEADER + feedDescription[0].get_text() + bcolors.ENDC)
+    feedDescription.pop(0)
+
+    item_num = len(feedTitle)
+
     if item_num == 0:
         print("Sorry, no torrents found.")
         sys.exit(0)
 
     for i in range(item_num):
-        item = feed["items"][i]
-        title = item['title'][:string_max]
-        # à refaire avec .format() pour afficher les noms des torrents
+        title = feedTitle[i].get_text()
+        description = feedDescription[i].get_text().split()
+        # We parse something like
+        # <description>Size: 4780 MB Seeds: 27 Peers: 17 Hash:
+        # a000000a00a0aaaa00aa0000a0aaa0a0000aa0aa </description>
 
-        if item['title'].__len__() < string_max:
-            title += (string_max-item['title'].__len__()) * ' '
-        v = item['summary_detail']['value']
-        # pattern matching de size, seeds & peers a la va-vite
-        size = v['Size: '.__len__(): v.find("Seed")].strip()
-        seeds = v[v.find("Seeds")+'Seeds: '.__len__(): v.find("Peer")]
-        peers = v[v.find("Peers")+'Peers: '.__len__(): v.find("Hash")]
-        if (option.team is None) or (option.team in title.lower()):
-            printstr = "%d:\t%s \t\t (%s)" \
-                + "S: " + bcolors.BOLD + bcolors.RED + "%s" + bcolors.ENDC \
-                + " P: " + bcolors.BOLD + bcolors.GREEN + "%s" + bcolors.ENDC
-            print(printstr % (i, title, size, seeds, peers))
+        size = description[1]
+        seeds = description[4]
+        peers = description[6]
+
+        print("{:3d}) {:50s}: Size: {:4s} MB Seeds: {:3s}  Peers: {:3s}".format(
+            i,
+            title if title < 50 else title[:50],
+            size,
+            bcolors.GREEN + seeds + bcolors.ENDC,
+            bcolors.RED + peers + bcolors.ENDC))
 
     sys.stdout.write("Which torrent to retrieve ? (or q to quit) : ")
     torrent = sys.stdin.readline()
@@ -197,10 +206,10 @@ def main(option, domain):
         print("Exiting the client.")
         sys.exit(0)
 
-    trackerindex = feed['items'][int(torrent)]['link']
-    trackerindex = trackerindex.replace("node21", "www.torrentz.com")
-    # new modifiction on RSS urls -- 20100816
-    title = feed['items'][int(torrent)]['title']
+    trackerindex = feed.find_all('guid')[int(torrent)].get_text()
+    # trackerindex = trackerindex.replace("node21", "www.torrentz.com")
+
+    title = feedTitle[int(torrent)].get_text()
     title = title.replace(' ', '_').replace('/', '_')
     # formatting for saved torrent filename
 
@@ -210,7 +219,7 @@ def main(option, domain):
     # trackers var contain the source of the torrentz selectioned torrent page
 
     outputPath = destdir + '/' + title + '.torrent'
-    outputPath = '/home/thomacer/Downloads/' + title + '.torrent'
+    # outputPath = '/home/thomacer/Downloads/' + title + '.torrent'
 
     hit = False
     downloadLocationTest = locationTesting(trackers, outputPath, title)
@@ -228,8 +237,6 @@ if __name__ == "__main__":
                         help='Show more information the execution')
     parser.add_argument('-d', '--destdir', default='~/Downloads', type=str,
                         help='Destination of the downloaded torrent')
-    parser.add_argument('-t', '--team', default=None, type=str,
-                        help='')
     parser.add_argument('-n', '--no_verified', default=False,
                         action='store_true',
                         help='Option to do unverified search')
